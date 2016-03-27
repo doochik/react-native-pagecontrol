1d8d399fbfeca08321e532ed0b78445fe0cd544a
Aleksei Androsov <doochik@ya.ru>
2016-03-27 12:01:18 +0300

    Create empty android fallback

---
+++ PageControlIOS.android.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @providesModule PageControlIOS
+ */
+
+import React from 'react-native';
+
+class PageControlIOS extends React.Component {
+
+    render() {
+        return null;
+    }
+
+}
+
+module.exports = PageControlIOS;
